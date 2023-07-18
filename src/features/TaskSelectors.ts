ebba56e6b23e51a5febdb37b6fb891723026b541
Limninzz <alimerlianmubarok@gmail.com>
2023-07-18 19:24:37 +0700

    Refacotr raw data

--- src/features/TaskSelectors.ts
+++ src/features/TaskSelectors.ts
@@ -1,9 +1,11 @@
 import { selector } from 'recoil'
 import { tasksState } from './TaskAtoms'
 import type { Task } from '../types'
+import {SelectorKeys} from '../constants/recoilKeys'
 
 export const uncompletedTasksSelector = selector<Task[]>({
-  key: 'uncompleted_tasks',
+  //key: 'uncompleted_tasks',
+  key: SelectorKeys.UNCOMPLETED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder !== 4
@@ -12,7 +14,8 @@ export const uncompletedTasksSelector = selector<Task[]>({
 })
 
 export const completedTasksSelector = selector<Task[]>({
-  key: 'completed_tasks',
+  //key: 'completed_tasks',
+  key: SelectorKeys.COMPLETED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 4
@@ -21,7 +24,8 @@ export const completedTasksSelector = selector<Task[]>({
 })
 
 export const notStartedTasksSelector = selector<Task[]>({
-  key: 'not_started_task',
+  //key: 'not_started_task',
+  key: SelectorKeys.NOT_STARTED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 1
@@ -30,7 +34,8 @@ export const notStartedTasksSelector = selector<Task[]>({
 })
 
 export const inProgressTasksSelector = selector<Task[]>({
-  key: 'in_progress_task',
+  //key: 'in_progress_task',
+  key: SelectorKeys.IN_PROGRESS_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 2
@@ -39,7 +44,8 @@ export const inProgressTasksSelector = selector<Task[]>({
 })
 
 export const waitingTasksSelector = selector<Task[]>({
-  key: 'waiting_task',
+  //key: 'waiting_task',
+  key: SelectorKeys.WAITING_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 3
